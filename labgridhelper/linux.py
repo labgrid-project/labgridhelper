labgrid-project/labgridhelper

commit fcdeab4d3ea3c25d821e3fa558f4e49749778a87

linux: add helpers from InfoDriver
This ports the functionality of the InfoDriver in labgrid to this library. The
intention is to remove the InfoDriver from labgrid eventually.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgridhelper/linux.py
@@ -57,3 +57,64 @@ def get_commands(command, directories=None):
                 commands.append(cmd)
 
     return commands
+
+def get_systemd_service_active(command, service):
+    """Returns True if service is active, False in all other cases
+    Args:
+        command (CommandProtocol): An instance of a Driver implementing the CommandProtocol
+        service (str): name of the service
+    Returns:
+        bool: True if service is active, False otherwise
+    """
+    assert isinstance(command, CommandProtocol), "command must be a CommandProtocol"
+    _, _, exitcode = command.run(
+        "systemctl --quiet is-active {}".format(service)
+    )
+    return exitcode == 0
+
+def get_interface_ip(command, interface="eth0"):
+    import re
+    """Returns the global valid IPv4 address of the supplied interface
+    Args:
+        command (CommandProtocol): An instance of a Driver implementing the CommandProtocol
+        interface (string): name of the interface
+    Returns:
+        str: IPv4 address of the interface, None otherwise
+    """
+    try:
+        ip_string = command.run_check("ip -o -4 addr show")
+    except ExecutionError:
+        self.logger.debug('No ip address found')
+        return None
+
+    regex = re.compile(
+        r"""\d+:       # Match the leading number
+        \s+(?P<if>\w+) # Match whitespace and interfacename
+        \s+inet\s+(?P<ip>[\d.]+) # Match IP Adress
+        /(?P<prefix>\d+) # Match prefix
+        .*global # Match global scope, not host scope""", re.X
+    )
+    result = {}
+
+    for line in ip_string:
+        match = regex.match(line)
+        if match:
+            match = match.groupdict()
+            result[match['if']] = match['ip']
+    if result:
+        return result[interface]
+
+    return None
+
+def get_hostname(command):
+    """Returns the hostname
+    Args:
+        command (CommandProtocol): An instance of a Driver implementing the CommandProtocol
+    Returns:
+        str: hostname of the target, None otherwise
+    """
+    try:
+        hostname_string = command.run_check("hostname")
+    except ExecutionError:
+        return None
+    return hostname_string[0]

commit abd44360d28be631ae89930ad8c0395cc8a44f93

linux: make sure ExecutionError is defined
ExecutionError is used all over the place in this file, but python is
not happy about it:

    >       except ExecutionError:
    E       NameError: name 'ExecutionError' is not defined

    labgridhelper/labgridhelper/linux.py:173: NameError

Fixes: fcdeab4d3ea3c25d821e (2019-02-27, "linux: add helpers from InfoDriver")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>

labgridhelper/linux.py
@@ -1,4 +1,6 @@
 from labgrid.protocol import CommandProtocol
+from labgrid.driver import ExecutionError
+
 import json
 import re
 